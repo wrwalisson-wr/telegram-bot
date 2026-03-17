794d10adbeef5cbb4d0d729d36f128fbca27fd6c
wrwalisson-wr <wr.walisson@gmail.com>
2026-03-17 20:09:42 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTyp
 from openai import OpenAI
 
 TOKEN = os.getenv("8712222565:AAH9ajBX11Ky_-H5L6w1ZCQSa65NUbR2-cc")
-client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+client = OpenAI(api_key=os.getenv("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"))
 
 async def responder(update: Update, context: ContextTypes.DEFAULT_TYPE):
     texto = update.message.text
